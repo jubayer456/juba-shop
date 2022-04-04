b836689f5a413a3a4a327306b4275d9235704f3b
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-04-05 00:10:10 +0600

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home'
+import NotFound from './components/NotFound/NotFound'
+import Reviews from './components/Reviews/Reviews'
+import Blogs from './components/Blogs/Blogs'
+import DashBoard from './components/DashBoard/DashBoard'
+import About from './components/About/About'
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='/dashboards' element={<DashBoard />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/about' element={<About />}></Route >
+        <Route path='*' element={<NotFound />}></Route>
+
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light ">
+        <nav class="navbar navbar-expand-lg navbar-light py-3 ">
             <div class="container">
+                <Link to='/'>Juba Shop</Link>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mx-auto">
+                    <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
                             <Link to='/'>Home</Link>
                         </li>
