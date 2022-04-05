167afcb4a7c72237391f4303c4f7b0c55ea6b561
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-04-05 08:26:14 +0600

    add blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs</h1>
+        <div className='container'>
+            <h2>1. What is context Api?</h2>
+            <p>The React Context API is a way to effectively create global variables for a React app that can be passed around. This is an alternative to "prop drilling" or moving props from grandparents to children to parents.</p>
+            <h2>What is semantic tag?</h2>
+            <p>The HTML5 semantic tag determines the purpose of the element. By using semantic markup,the browser understand the meaning of the content instead of just displaying it. By providing this extra level of clarity, HTML5 semantic elements help search engines read the page and quickly find the information they need.</p>
+
         </div>
     );
 };
