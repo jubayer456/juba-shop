abfc7050c297b26a2668b7f6f02a903582d087f0
jubayer456 <65710197+jubayer456@users.noreply.github.com>
2022-06-04 11:44:05 +0600

    new path

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='text-center'>
-            <h1>Policy</h1>
+            <h1>Policy Of Our Company</h1>
             <p>Company policies are guidelines that help employers deal with the health, safety and accountability of employees, as well as their interactions with customers or clients. Business policies can also be used as a guideline for federal or state regulatory requirements, legal issues and other situations that can lead to severe consequences for employees.</p>
         </div>
     );
